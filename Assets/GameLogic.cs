3145233a3d6e26afb7d475e786491460b575f6e0
Ilia Onishchenko <ilia_onishchenko@epam.com>
2017-10-04 23:48:14 +0300

    Implement pure artificial intelligence
    But program stil does not compile

--- Assets/GameLogic.cs
+++ Assets/GameLogic.cs
@@ -2,6 +2,7 @@
 using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 public class GameLogic : MonoBehaviour {
 
@@ -45,9 +46,115 @@ public class GameLogic : MonoBehaviour {
 
 	private void AI(out int i, out int j) // i,j - place, where AI decided to instantiate circle or cross
 	{
-		
+		var nextMove = minimax (field, Turn.AI, null);
+		i = nextMove.pair.x;
+		j = nextMove.pair.y;
+	} 
+
+	private int score(State state)
+	{
+		if (state == State.PlayerWin)
+			return -10;
+		else if (state == State.AIWin)
+			return 10;
+		else
+			return 0;
 	}
 
+	private Dictionary<Pair, Cell> unfoldMap(Cell[,] board)
+	{
+		Dictionary<Pair, Cell> map = new Dictionary<Pair, Cell> ();
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < columns; j++) {
+				map.Add (new Pair (i, j), board[i, j]);
+			}
+		}
+		return map;
+	}
+
+	private bool decision(Dictionary<int, Cell> board, Cell player)
+	{
+		if (
+			(board [0] == player && board [1] == player && board [2] == player) ||
+			(board [3] == player && board [4] == player && board [5] == player) ||
+			(board [6] == player && board [7] == player && board [8] == player) ||
+			(board [0] == player && board [3] == player && board [6] == player) ||
+			(board [1] == player && board [4] == player && board [7] == player) ||
+			(board [2] == player && board [5] == player && board [8] == player) ||
+			(board [0] == player && board [4] == player && board [8] == player) ||
+			(board [2] == player && board [4] == player && board [6] == player))
+			return true;
+		else
+			return false;
+	}
+
+
+	private Move minimax(Cell[,] board, Turn player, Pair lastPair)
+	{
+		var moves = new List<Move>();
+		var map = unfoldMap (board);
+
+		int res = score(
+			getState(
+				decision (map.ToDictionary (p => p.Key.unfold (), p => p.Value), makeMoveByTurn (player)),
+				player)
+		);
+
+		if (lastPair != null) {
+			if (res != 0)
+				return new Move (lastPair, res);
+			if (!map.Values.Contains (Cell.None))
+				return new Move(lastPair, 0);
+		}
+
+
+		var availableMoves = map.Where (m => m.Value == Cell.None).ToDictionary (m => m.Key, m => m.Value).Keys.ToList();
+
+		foreach (Pair pair in availableMoves) {
+			
+			Move move;
+
+			var updatedBoard = board;
+			updatedBoard [pair.x, pair.y] = makeMoveByTurn (player);
+			if (player == Turn.AI) {
+				move = minimax (updatedBoard, Turn.Player, pair);
+			} else {
+				move = minimax (updatedBoard, Turn.AI, pair);
+			}
+
+			moves.Add (move);
+		}
+
+		Move bestMove;
+
+		if (player == Turn.AI) {
+			var maxScore = moves.Max (m => m.score);
+			bestMove = moves.Where (m => m.score == maxScore).First ();
+		} else {
+			var minScore = moves.Min (m => m.score);
+			bestMove = moves.Where (m => m.score == minScore).First ();
+		}
+
+		return bestMove;
+	}
+
+	private Cell makeMoveByTurn(Turn turn)
+	{
+		if (turn == Turn.AI)
+			return Cell.Black;
+		else
+			return Cell.White;
+	}
+
+	private State getState(bool decision, Turn turn)
+	{
+		if (decision && turn == Turn.AI)
+			return State.AIWin;
+		else if (decision && turn == Turn.Player)
+			return State.PlayerWin;
+		else
+			return State.InProgress;
+	}
 
 	// Use this for initialization
 	void Start () {
@@ -73,6 +180,33 @@ public class GameLogic : MonoBehaviour {
 
 }
 
+class Pair{
+	public int x;//i
+	public int y;//j
+	public Pair(int x, int y)
+	{
+		this.x = x;
+		this.y = y;
+	}
+
+	public int unfold(){
+		return x * 3 + y;
+	}
+}
+
+class Move
+{
+	public Pair pair;
+	public int score;
+	public Move(Pair pair, int score)
+	{
+		this.pair = pair;
+		this.score = score;
+	}
+}
+
 enum Cell {White, Black, None};
 
 enum Turn{Player, AI};
+
+enum State{PlayerWin, AIWin, InProgress}
